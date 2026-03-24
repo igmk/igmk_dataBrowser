eb5eb4df03a5a61d5772ca653656aee0a27bbfc5
Marcus Müller <22216795+marcusgmueller@users.noreply.github.com>
2026-03-24 11:15:03 +0100

    Simplify entrypoint.sh by removing apiServerURL
    Remove unused variable and streamline envsubst usage.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-export apiSeverURL
-
-envsubst '${apiSeverURL}' < /usr/share/nginx/html/dataBrowser.js > /usr/share/nginx/html/dataBrowser.js.tmp
-mv /usr/share/nginx/html/dataBrowser.js.tmp /usr/share/nginx/html/dataBrowser.js
+envsubst < /usr/share/nginx/html/dataBrowser.js.temp > /usr/share/nginx/html/dataBrowser.js
 
 nginx -g 'daemon off;'

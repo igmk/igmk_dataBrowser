011a8cf6fd838f1191c80a80aa715ec78a3032d0
Luca Sommer <luca.l.sommer@protonmail.com>
2026-05-07 12:45:07 +0200

    Fix env var bug in map

--- dataBrowser/map/leaflet/map.js
+++ dataBrowser/map/leaflet/map.js
@@ -1,6 +1,6 @@
 let map = L.map('map').setView([50.8, 6.6], 10);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
-fetch("$APIURL/sites")
+fetch("https://api.herz-campaigns.de/sites")
 .then(res => {
     if (!res.ok) throw new Error("HTTP " + res.status);
     return res.json();

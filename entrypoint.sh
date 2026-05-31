61678a81b46abe6c5c464ed8d3082b90d843400d
Luca Sommer <luca.l.sommer@protonmail.com>
2026-05-31 13:27:40 +0200

    Only substitute the API URL, not other templates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-envsubst < /usr/share/nginx/html/dataBrowser.js.temp > /usr/share/nginx/html/dataBrowser.js
+# Only substitute $APIURL. Without the explicit list, envsubst would also clobber
+# JavaScript template-literal interpolations like ${panel}, ${label}, ${item}.
+envsubst '$APIURL' < /usr/share/nginx/html/dataBrowser.js.temp > /usr/share/nginx/html/dataBrowser.js
 
 nginx -g 'daemon off;'

d81c4ee3c1ecc01dc3ae36b120523cb3acfcb06b
Luca Sommer <luca.l.sommer@protonmail.com>
2026-06-07 13:34:01 +0200

    Merge subsites into one map marker

--- dataBrowser/map/leaflet/map.js
+++ dataBrowser/map/leaflet/map.js
@@ -1,5 +1,13 @@
 let map = L.map('map').setView([50.8, 6.6], 10);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
+
+// Sub-sites folded into a parent marker's combined multi-site view; no own marker.
+const hiddenSites = new Set(["cgn_bio", "jue_tower"]);
+// Marker site id -> the '+'-joined site list its link should open.
+const siteGroups = {
+    cgn_city: "cgn_city+cgn_bio",
+    jue: "jue+jue_tower",
+};
 fetch("https://api.herz-campaigns.de/sites")
 .then(res => {
     if (!res.ok) throw new Error("HTTP " + res.status);
@@ -9,6 +17,8 @@ fetch("https://api.herz-campaigns.de/sites")
 
     Object.keys(sites).forEach(id => {
 
+        if (hiddenSites.has(id)) return; // folded into a parent marker's combined view
+
         fetch(`https://api.herz-campaigns.de/sites/${id}`)
         .then(res => {
             if (!res.ok) throw new Error("HTTP " + res.status);
@@ -16,7 +26,8 @@ fetch("https://api.herz-campaigns.de/sites")
         })
         .then(site => {
 
-            const siteUrl = `https://browser.herz-campaigns.de/?ms=${id}%2C`;
+            const msValue = (siteGroups[id] || id) + ",";
+            const siteUrl = `https://browser.herz-campaigns.de/?ms=${encodeURIComponent(msValue)}`;
 
             L.marker([site.lat, site.lon])
             .addTo(map)
